b008bcb1c72999bc212b02fc010530c744e448f6
Erik Nilsson <nilsson.erik.k@gmail.com>
2025-08-31 18:02:05 +0200

    trying to fix issues

--- rust_audio_processor/src/alsa_processor.rs
+++ rust_audio_processor/src/alsa_processor.rs
@@ -164,7 +164,7 @@ impl AlsaAudioProcessor {
         let mut frames_processed = 0;
         while *is_running.read() {
             // Read input using the correct ALSA API
-            match input_io.read(&mut input_buffer) {
+            match input_io.readi(&mut input_buffer) {
                 Ok(_) => {
                     // Process audio through stereo delay
                     if let Ok(mut delay) = stereo_delay.lock() {
@@ -187,7 +187,7 @@ impl AlsaAudioProcessor {
                     }
                     
                     // Write output using the correct ALSA API
-                    if let Err(e) = output_io.write(&output_buffer) {
+                    if let Err(e) = output_io.writei(&output_buffer) {
                         eprintln!("Output write error: {}", e);
                     }
                     
